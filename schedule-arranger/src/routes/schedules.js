e194e40e9db3b3eb7fee5cf037f81346e464c6e9
akatak998 <257zs16101@student.zen.ac.jp>
2026-08-18 17:45:02 +0900

    出欠が機能するように修正

--- schedule-arranger/src/routes/schedules.js
+++ schedule-arranger/src/routes/schedules.js
@@ -307,7 +307,7 @@ app.post(
     // Upsert (存在すれば更新、なければ作成)
     await prisma.availability.upsert({
       where: {
-        candidateId_userId: { // availabilityIdをcandidateId_userIdに変更
+        availabilityCompositeId: { // availabilityIdをcandidateId_userIdに変更し、candidateId_userIdをavailabilityCompositeIdに変更
           candidateId: data.candidateId,
           userId: data.userId,
         },
