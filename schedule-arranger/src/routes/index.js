f0ddc71a4fcee16704435058b1f42ad3099022bc
akatak998 <257zs16101@student.zen.ac.jp>
2026-08-19 14:20:51 +0900

    予定を作るにアイコンを追加

--- schedule-arranger/src/routes/index.js
+++ schedule-arranger/src/routes/index.js
@@ -65,7 +65,7 @@ schedules.forEach((schedule) => {
           ? html`
               <div class="my-3">
                 <h3 class="my-3">予定を作る</h3>
-                <a class="btn btn-primary" href="/schedules/new">予定を作る</a>
+                <a class="btn btn-primary" href="/schedules/new">予定を作る <i class="bi bi-calendar-week"></i></a>
                 ${schedules.length > 0
                   ? html`
                       <h3 class="my-3">あなたの作った予定一覧</h3>
